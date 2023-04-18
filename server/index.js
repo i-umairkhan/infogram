d214e2cf4f50b62c9c5fa1b64ab2fedd2100a062
umair2607 <umair.k2607@gmail.com>
2023-04-18 06:41:49 +0500

    added posts routes + completed api

--- server/index.js
+++ server/index.js
@@ -10,7 +10,9 @@ import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
+import postsRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
 
 // CONFIGURATIONS
@@ -45,11 +47,13 @@ const upload = multer({ storage });
 // ROUTES WITH FILES
 
 app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 // ROUTES
 
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
+app.use("/posts", postsRoutes);
 
 // MONGOOSE SETUP
 
